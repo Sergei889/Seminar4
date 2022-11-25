cfcb73f3759d619ebaa3113f7ee1f8fa304922b9
Sergei.S <sannikov.sergei889@yandex.ru>
2022-11-25 13:59:28 +0300

    Выполнена задача 29

---
+++ proekt2/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+ и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] getRange( int length)
+{
+    int [] array = new int [length];
+    Random rd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rd.Next(0, 999);
+    }
+    return array;
+}
+
+void printArray( int [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int [] array = getRange(8);
+printArray(array);
