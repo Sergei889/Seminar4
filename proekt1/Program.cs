55889e083a7474f2971cb440496cd099203374ba
Sergei.S <sannikov.sergei889@yandex.ru>
2022-11-25 13:46:54 +0300

    Выполнена задача 27

---
+++ proekt1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+int number = ReadInt("Введите число: ");
+int N = NumberN(number);
+SumNumbers(number, N);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberN(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int N)
+{
+    int sum = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
